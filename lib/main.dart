1d088f4cc241098a276c6e05b64652d08b4de66a
dbdmsdn10 <dbdmsdn10@naver.com>
2021-02-02 09:24:06 +0900

    안드로이드에 push기능 넣음

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:permission_handler/permission_handler.dart';
 
 
 import 'dart:async';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 import 'package:http/http.dart' as http;
 
@@ -13,6 +14,95 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+
+//여기서 부터 firebase알람용
+final Map<String, Item> _items = <String, Item>{};
+
+Item _itemForMessage(Map<String, dynamic> message) {
+  final dynamic data = message['notification'] ?? message;
+
+  final String itemId = data['title'];
+  final Item item = _items.putIfAbsent(itemId, () => Item(itemId: itemId))
+    ..status = data['body'];
+
+  return item;
+}
+
+class Item {
+  Item({this.itemId});
+
+  final String itemId;
+
+  StreamController<Item> _controller = StreamController<Item>.broadcast();
+
+  Stream<Item> get onChanged => _controller.stream;
+
+  String _status;
+
+  String get status => _status;
+
+  set status(String value) {
+    _status = value;
+    _controller.add(this);
+  }
+
+  static final Map<String, Route<void>> routes = <String, Route<void>>{};
+
+  Route<void> get route {
+    final String routeName = '/detail/$itemId';
+    return routes.putIfAbsent(
+      routeName,
+          () => MaterialPageRoute<void>(
+        settings: RouteSettings(name: routeName),
+        builder: (BuildContext context) => DetailPage(itemId),
+      ),
+    );
+  }
+}
+
+class DetailPage extends StatefulWidget {
+  DetailPage(this.itemId);
+
+  final String itemId;
+
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  Item _item;
+  StreamSubscription<Item> _subscription;
+
+  @override
+  void initState() {
+    super.initState();
+    _item = _items[widget.itemId];
+    _subscription = _item.onChanged.listen((Item item) {
+      if (!mounted) {
+        _subscription.cancel();
+      } else {
+        setState(() {
+          _item = item;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Item ${_item.itemId}"),
+      ),
+      body: Material(
+        child: Center(child: Text("Item status: ${_item.status}")),
+      ),
+    );
+  }
+}
+//firebase 알람용 아래는 화면 만드는용
+//여기까지아 firebase용
 
 
 class MyApp extends StatelessWidget {
@@ -52,6 +142,92 @@ class _MyHomePageState extends State<MyHomePage> {
   TextEditingController _inputController = new TextEditingController();
   TextEditingController _outputController = new TextEditingController();
   String message2 = "";
+
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+  //앱켜져있을때 알람받으면 dialog로 보여주는용
+  Widget _buildDialog(BuildContext context, Item item) {
+    return AlertDialog(
+      content: Text("Item ${item.itemId} has been updated"),
+      actions: <Widget>[
+        FlatButton(
+          child: const Text('CLOSE'),
+          onPressed: () {
+            Navigator.pop(context, false);
+          },
+        ),
+        FlatButton(
+          child: const Text('SHOW'),
+          onPressed: () {
+            Navigator.pop(context, true);
+          },
+        ),
+      ],
+    );
+  }
+
+  void _showItemDialog(Map<String, dynamic> message) {
+    showDialog<bool>(
+      context: context,
+      builder: (_) => _buildDialog(context, _itemForMessage(message)),
+    ).then((bool shouldNavigate) {
+      if (shouldNavigate == true) {
+        //보기 누를시
+        //_navigateToItemDetail(message);//다른화면을 열며 내용을 보여준다
+        final dynamic data = message['notification'] ?? message;
+
+        setState(() {
+          message2 = "제목: " + data['title'] + "\n내용: " + data['body'];
+        });
+      }
+    });
+  }
+
+  void _navigateToItemDetail(Map<String, dynamic> message) {
+    // 내용 보여줄 화면 만들기로 추정됨
+    final Item item = _itemForMessage(message);
+    // Clear away dialogs
+    Navigator.popUntil(context, (Route<dynamic> route) => route is PageRoute);
+    if (!item.route.isCurrent) {
+      Navigator.push(context, item.route);
+    }
+  }
+
+  @override
+  void initState() {
+    //앱 켜져 있을시 직접 메세지 받기
+    super.initState();
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+        _showItemDialog(message);
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+        _navigateToItemDetail(message);
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+        _navigateToItemDetail(message);
+      },
+    );
+    _firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(
+            sound: true, badge: true, alert: true, provisional: true));
+    _firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings settings) {
+      print("Settings registered: $settings");
+    });
+    _firebaseMessaging.getToken().then((String token) {
+      assert(token != null);
+      setState(() {
+        _homeScreenText = "Push Messaging token: $token";
+      });
+      print(_homeScreenText);
+    });
+  }
+
+//여기까지가 firebase 메세지 받으면 사용하는  함수들
 
   @override
   Widget build(BuildContext context) {
